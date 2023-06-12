8dc7d09f30e6f4e74d07942a27556ae7e6c313c1
Ritik Raj <ritik2358@gmail.com>
2023-06-13 01:39:55 +0530

    added more functionalities

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 let $ = document
 let input = $.getElementById('input')
 let buttons = $.querySelectorAll("button")
-const oparatorChar = ["%", "*", "/", "+", "-", "="]
+const oparatorChar = ["%", "*", "/", "+", "-", "=", "sqrt", "log", "ln"]
 let output = ''
 
 const calculate = (btnValue) => {
-    if(btnValue === '=' && output !== ''){
+    if (btnValue === '=' && output !== '') {
         output = eval(output.replace("%", '/100'))
-    } else if (btnValue ==="AC"){
+    }
+    else if (btnValue === "√") {
+        output = Math.sqrt(output)
+    }
+    else if (btnValue === "log") {
+        output = Math.log10(output)
+    }
+    else if (btnValue === "ln") {
+        output = Math.log(output)
+    }
+    else if (btnValue === "square") {
+        output = Math.pow(output, 2)
+    }
+    else if (btnValue === "AC") {
         output = ''
-    } else if (btnValue === "DEL"){
-        output = output.toString().slice(0,-1)
-    }else {
-        if (output ==='' && oparatorChar.includes(btnValue)) {
+    } else if (btnValue === "DEL") {
+        output = output.toString().slice(0, -1)
+    }
+    else {
+        if (output === '' && oparatorChar.includes(btnValue)) {
             return
         }
         output += btnValue
@@ -23,11 +37,11 @@ const calculate = (btnValue) => {
 }
 
 buttons.forEach(function (button) {
-    button.addEventListener('click', (e)=> {
+    button.addEventListener('click', (e) => {
         calculate(e.target.value)
     })
 })
 
-window.addEventListener('load', ()=> {
+window.addEventListener('load', () => {
     input.value = JSON.parse(localStorage.getItem('result'))
 });
